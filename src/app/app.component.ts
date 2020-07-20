2a872d4a2197f1dbe3cd5bc6afd7fe2e996a24d3
Judy Lipinski <judy@karmoxie.com>
2020-07-20 07:09:39 -0400

    add example interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { Person, RestrictedPerson } from "./shared/models/person.model";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  template: `
-    <p>Name: {{ person.name }}</p>
-    <p>Email: {{ person.email }}</p>
-    <p>Age: {{ person.age }}</p>
-    <h2>Restricted Person</h2>
-    <p>Age {{restrictedPerson.age}}
-  `
+    selector: 'app-root',
+    template: `
+        <h1>Welcome to {{ title }} in {{ theYear }}</h1>
+        <p>Next year is {{ 1 + theYear }}</p>
+    `,
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  person: Person;
-  restrictedPerson: RestrictedPerson;
+    // literals dont need types
+    title = 'angular100-demos';
+    isTrue = true;
 
-  ngOnInit(): void {
-    this.person = {
-      name: "Jane",
-      email: "alias@nbc.com",
-      age: 34
-    };
+    // use types with variables initialized in ngOnInit
+    theYear: number;
 
-    this.restrictedPerson = {...this.person,age:21}
-  }
-
-  someExample() {
-    const notSure: any = 4;
-    notSure.ifItExists(); // okay, ifItExists might exist at runtime
-    notSure.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
-
-    const prettySure: Object = 4;
-    //  prettySure.toFixed();
-    // Error: Property 'toFixed' doesn't exist on type 'Object'.
-  }
+    ngOnInit(): void {
+        this.theYear = new Date().getFullYear();
+    }
 }
