24ec26f58004535a34bd5079d0ece0cfe4b82c91
Judy Lipinski <judy@karmoxie.com>
2020-07-20 11:15:03 -0400

    Use @Input() to pass from list to person

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -3,16 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-people-list',
   template: `
-    <app-person></app-person>
-    <app-person></app-person>
-    <app-person></app-person>
+  <div class="container">
+      <div class="card-deck">
+        <app-person [name]="peopleArray[0]"></app-person>
+        <app-person [name]="peopleArray[1]"></app-person>
+        <app-person [name]="peopleArray[2]"></app-person>
+      </div>
+    </div>
   `
 })
 export class PeopleListComponent implements OnInit {
 
-  constructor() { }
+    peopleArray: any[];
 
-  ngOnInit() {
+    constructor() { }
+
+    ngOnInit(): void {
+      this.peopleArray = ['April', 'May', 'June'];
+    }
+
   }
-
-}
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-person',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
+  @Input()
+  name: string;
 
   constructor() { }
 
