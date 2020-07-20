458dcb9a8db186052b43fc4346997f89f3ef44f6
Judy Lipinski <judy@karmoxie.com>
2020-07-20 07:12:11 -0400

    Add safe operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-root',
     template: `
-        <h1>Welcome to {{ title }} in {{ theYear }}</h1>
-        <p>Next year is {{ 1 + theYear }}</p>
-    `,
+    <p>Is {{ somePerson?.name }} here?</p>
+  `,
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-    // literals dont need types
-    title = 'angular100-demos';
-    isTrue = true;
-
-    // use types with variables initialized in ngOnInit
-    theYear: number;
-
-    ngOnInit(): void {
-        this.theYear = new Date().getFullYear();
-    }
+export class AppComponent  {
+  somePerson: any;
 }
