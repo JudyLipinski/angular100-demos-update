1ef227d10e128b0ce79d9897efddc8c0ea047721
Judy Lipinski <judy@karmoxie.com>
2020-07-20 07:45:12 -0400

    Add ch detection example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: `
-    <p>Is {{ somePerson?.name }} here?</p>
-  `,
-    styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  template: `
+          <p [hidden]="isHidden">Is it hidden?</p>
+          `,
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  somePerson: any;
+export class AppComponent implements OnInit {
+  isHidden: boolean;
+
+  ngOnInit(): void {
+    this.isHidden = false;
+
+    setInterval(() => { this.isHidden = !this.isHidden; }, 1000);
+  }
 }
+
