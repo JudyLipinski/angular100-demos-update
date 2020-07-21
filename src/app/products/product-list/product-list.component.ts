ac62ea98115c1229dac74b755934ba4da85ffba1
Judy Lipinski <judy@karmoxie.com>
2020-07-21 15:39:21 -0400

    workgin on if

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,31 +7,38 @@ import { Product } from './product';
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   products: Product[];
+  productId = 3;
   intervalId: any;
   differentQty = 10;
 
   ngOnInit(): void {
-    this.products = [
-      { id: 1423, description: 'cups', quantity: 30 },
-      { id: 1232, description: 'spoons', quantity: 10000 },
-      { id: 353, description: 'knives', quantity: 0 },
-      { id: 324, description: 'notebooks', quantity: 15 },
-      { id: 1435, description: 'bottles', quantity: 3 },
-      { id: 126, description: 'crystals', quantity: 1 }
-    ];
 
-    this.intervalId = setInterval(() => {
+    setTimeout(() => {
       this.products = [
-        { id: 1423, description: 'cups', quantity: this.differentQty++ },
+        { id: 1423, description: 'cups', quantity: 30 },
         { id: 1232, description: 'spoons', quantity: 10000 },
         { id: 353, description: 'knives', quantity: 0 },
-        { id: 324, description: 'notebooks', quantity: this.differentQty++ },
+        { id: 324, description: 'notebooks', quantity: 15 },
         { id: 1435, description: 'bottles', quantity: 3 },
         { id: 126, description: 'crystals', quantity: 1 }
       ];
 
+      this.intervalId = setInterval(() => {
+        this.products = [
+          { id: 1423, description: 'cups', quantity: this.differentQty++ },
+          { id: 1232, description: 'spoons', quantity: 10000 },
+          { id: 353, description: 'knives', quantity: 0 },
+          { id: 324, description: 'notebooks', quantity: this.differentQty++ },
+          { id: 1435, description: 'bottles', quantity: 3 },
+          { id: 126, description: 'crystals', quantity: 1 }
+        ];
 
-    }, 1000);
+      }, 1000);
+
+    }, 5000);
+
+
+
 
     setTimeout(() => { clearInterval(this.intervalId); }, 20000);
 
