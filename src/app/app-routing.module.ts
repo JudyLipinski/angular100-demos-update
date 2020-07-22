e4ed069282033e2c15bb1776612d91e0aa55a4c4
Judy Lipinski <judy@karmoxie.com>
2020-07-22 14:23:29 -0400

    work on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-//#region Import all referenced components
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { NotfoundComponent } from './notfound.component';
-//#endregion
+import { ProductListComponent } from './products/product-list.component';
+import { UserListComponent } from './users/user-list.component';
+import { ObservablesExampleComponent } from './observables/observables-example.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
-  { path: "contact", component: ContactComponent },
-  { path: "**", component: NotfoundComponent }
+  { path: "users", component: UserListComponent },
+  { path: "products", component: ProductListComponent },
+  { path: "observables", component: ObservablesExampleComponent },
+  { path: "**", redirectTo: "/users" }
 ];
 
 @NgModule({
@@ -20,3 +20,5 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
+
+// { path: "users/:id", component: PersonDetailComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,32 @@ import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { PeopleListComponent } from './people-list/people-list.component';
+import { PersonComponent } from './data/person.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductRowComponent } from './products/product-row.component';
+import { UserListComponent } from './users/user-list.component';
+import { UserCardComponent } from './users/user-card.component';
+import { UserTableComponent } from './users/user-table.component';
+import { UserRowComponent } from './users/user-row.component';
+import { NgCodeExamplesComponent } from './ng-code-examples/ng-code-examples.component';
+import { TemperatureConverterPipe } from './temperature/temperature-converter.pipe';
+import { TempExampleComponent } from './temperature/temp-example.component';
+import { ShowNumbersComponent } from './show-numbers.component';
+import { ObservablesExampleComponent } from './observables/observables-example.component';
+import { EvensObservableComponent } from './observables/evens-observable.component';
+import { FromObservableComponent } from './observables/from-observable.component';
+import { OddsObservableComponent } from './observables/odds-observable.component';
+import { OperatorsObservableComponent } from './observables/operators-observable.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, NotfoundComponent, ContactComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, PeopleListComponent, PersonComponent, ProductListComponent, ProductRowComponent,UserListComponent, UserCardComponent, UserTableComponent, UserRowComponent, NgCodeExamplesComponent,TemperatureConverterPipe, TempExampleComponent, ShowNumbersComponent, FromObservableComponent, ObservablesExampleComponent, EvensObservableComponent, OddsObservableComponent, OperatorsObservableComponent,HomeComponent, NavbarComponent, NotfoundComponent, ContactComponent, FooterComponent],
+
+  imports: [BrowserModule, AppRoutingModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
