b2b877c13304c23a66e7ddf31d4f4c72a4e6bef1
Judy Lipinski <Judy@karmoxie.com>
2020-10-18 17:32:52 -0400

    improved example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,13 +25,12 @@ export class AppComponent implements OnInit {
     this.restrictedPerson = {...this.person,age:21}
   }
 
-  someExample() {
-    const notSure: any = 4;
-    notSure.ifItExists(); // okay, ifItExists might exist at runtime
-    notSure.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
+  someExample(): void {
+    const someVariable: any = 4;
+    someVariable.notARealFunction(); // no feedback, bad function
 
-    const prettySure: Object = 4;
-    //  prettySure.toFixed();
+    const somethingElse: Object = {};
+    somethingElse.toFixed();
     // Error: Property 'toFixed' doesn't exist on type 'Object'.
   }
 }
